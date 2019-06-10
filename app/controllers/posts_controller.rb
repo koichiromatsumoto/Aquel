b221e7fb75cd20b400bc9a31da61eda3642e8876
松本航一郎 <matsumotokouichirou@matsumotokouichirounoMacBook-Pro.local>
2019-06-10 20:17:55 +0900

    [add]投稿機能、ビューは後で

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,29 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+    @categories = Category.all
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    @categories = Category.all
+    session[:post_info] = @post
+
   end
 
   def create
+    @post = Post.new(session[:post_info])
+    @post.user_id = current_user.id
+    if params[:back]
+      render :new
+    elsif @post.save
+      redirect_to user_path(current_user.id)
+    else
+      render :new
+    end
+    session[:post_info] = nil
+    session[:post_image] = nil
+    session[:post_categories] = nil
   end
 
   def index
@@ -21,5 +42,11 @@ class PostsController < ApplicationController
   end
 
   def hashtag
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :post_image, :title, :text, category_ids:[])
   end
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,6 @@
 class Album < ApplicationRecord
   belongs_to :user
+
+  has_many :post_albums
+  has_many :posts, through: :post_albums
 end
